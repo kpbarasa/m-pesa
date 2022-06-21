5ec0cf438e7f9aa95b454f6b08d70d0a95d948df
kpbarasa <65596472+kpbarasa@users.noreply.github.com>
2022-06-21 15:58:30 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,267 @@
+# m-pesa
+m-pesa daraja api 
+
+<p>
+Simple eccommerce checkout back-end appllication with: Authentication, Shopping Cart processing, Sale transaction processing(PayPal, Stripe & M-Pesa API), Couppon discount options and Shipping processing.
+</p>
+
+1. Endpoints authentication
+   1. Authenticate user
+   2. Authenticate orders
+   3. Authenticate Sales trans actions 
+
+
+<h5>APP COMPONENETS :-<h5>
+<h5>1. NODE DEPENDENCIES =========================================================================<h5>
+<p>
+"@paypal/checkout-server-sdk": "^1.0.3",
+
+"axios": "^0.26.1",
+
+"body-parser": "^1.19.2",
+
+"cors": "^2.8.5",
+
+"dotenv": "^16.0.0",
+
+"ejs": "^3.1.6",
+
+"express": "^4.17.3",
+
+"mongoose": "^6.2.3",
+
+"request": "^2.88.2", 
+</p>
+
+<h5>
+<h5>
+Git ignore files ===============================================================================
+
+1.    node_module
+
+2.    .env
+
+<h5>
+
+<h5>Features present in the app :-<h5>
+
+<p>
+Simple eccommerce checkout back-end appllication with: Authentication, Shopping Cart processing, Sale transaction processing(PayPal, Stripe & M-Pesa API), Couppon discount options and Shipping processing.
+</p>
+
+1. Endpoints authentication
+   1. Authenticate user
+   2. Authenticate orders
+   3. Authenticate Sales trans actions 
+
+
+
+<h5>APPP COMPONENETS INDEX:-<h5> 
+
+1.NODE DEPENDENCIES
+
+2.ENV
+
+3.ROUTES/ENDPOINTS 
+
+ 
+<h5>APP COMPONENETS :-<h5>
+
+<h5>1. NODE DEPENDENCIES =========================================================================<h5>
+<p>
+"@paypal/checkout-server-sdk": "^1.0.3",
+
+"axios": "^0.26.1",
+
+"cookie-parser": "^1.4.6",
+
+"cors": "^2.8.5",
+
+"dotenv": "^16.0.0",
+
+"express": "^4.17.3",
+
+"mongoose": "^6.2.3",
+
+"request": "^2.88.2",
+
+"stripe": "^8.205.0"
+</p>
+
+<h5>
+<h5>
+
+1. Git ignore files ===============================================================================
+
+      node_module
+
+      .env
+
+<h5>
+
+2. env  ============================================================================
+</h5>
+
+      ATLAS_URI=mongodb+srv://<UserName>:<password>.4p1ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+      SAFARICOM_AUTH_TOKEN=<token here>
+      SAFARICOM_CLIENT_URL=https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials
+
+
+      COOKIE_SECRETE=<secrete here>
+    
+
+
+<h5>
+
+
+3. ROUTES/ENDPOINTS ============================================================================
+
+ Routes:
+
+</h5>
+<p> 
+
+3.1   Route:  /access_token (app.js)
+
+      type: Get
+
+      Response-:
+            {
+
+                  "access_token": "gGWQ7ytQQ5yR0nrGAL1TtJXzCoSY"
+
+            }
+
+3.2 Route:  /validation (app.js)
+
+      type: Post
+
+      Response-:
+            {
+
+                  "status_code": 200, // or 400
+
+                  "description": "Validated" // or "Failed"
+
+            }
+
+3.3 Route:  /register_url (app.js)
+
+      type: Get
+  
+      Response-:
+            {
+
+               "OriginatorCoversationID": "64441-43124039-1",
+               "ResponseCode": "0",
+               "ResponseDescription": "Accept the service request successfully."
+
+            } 
+
+3.4 Route:  /stk (app.js)
+
+      type: Get
+
+      Response-:
+            {
+
+               "ConversationID": "AG_20191219_000043fdf61864fe9ff5",
+               "OriginatorCoversationID": "16738-27456357-1",
+               "ResponseDescription": "Accept the service request successfully."
+
+            } 
+
+
+3.5 Route:  /bal_timeout (app.js)
+
+      type: Post
+
+      Response-:
+            {
+ 
+                  "requestId": "11728-2929992-1",
+                  "errorCode": "401.002.01",
+                  "errorMessage": "Error Occurred "
+
+            }
+
+3.6 Route:  /stk_callback  (app.js)
+
+      type: Get
+
+      Response-:
+            {
+  
+               "Body": {        
+
+                     "stkCallback": {     
+
+                     "MerchantRequestID": "29115-34620561-1",   
+
+                     "CheckoutRequestID": "ws_CO_191220191020363925", 
+                               
+                     "ResultCode": 0,            
+
+                     "ResultDesc": "The service request is processed successfully.",
+
+                     "CallbackMetadata": {         
+
+                     "Item": [{              
+
+                           "Name": "Amount",    
+
+                            "Value": 1.00      
+
+                           },                  
+
+                           {                  
+
+                              "Name": "MpesaReceiptNumber",  
+
+                              "Value": "NLJ7RT61SV"        
+
+                           },                    
+
+                           {                        
+
+                              "Name": "TransactionDate", 
+
+                              "Value": 20191219102115    
+
+                           },                   
+
+                           {                       
+
+                              "Name": "PhoneNumber",  
+
+
+                              "Value": 254708374149      
+
+                     }]            
+
+                     }        
+
+                     }    
+                   }
+
+            }
+
+3.9 function accessaccess
+      
+      function -: 
+
+                if (req.access_token = JSON.parse(body).access_token)
+
+                return next()
+      Response-:
+            {
+
+                  "access_token": "gGWQ7ytQQ5yR0nrGAL1TtJXzCoSY"
+
+            }
+</p>
+
+
+
+
---
+++ app.js
@@ -0,0 +1,271 @@
+const express = require('express')  
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const request = require('request')
+const app = express()  
+
+
+// Help connect to MongoDB database 
+const mongoose = require('mongoose');
+const { response } = require('express');
+// const { buffer } = require('stream/consumers');
+
+require('dotenv').config();
+
+// Set Port 
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
+// parse form data
+app.use(express.urlencoded({extended: false}))
+
+// Parse Json 
+app.use(express.json()) 
+
+app.use(bodyParser.json())
+
+// Connection String to express atlas and Environment variable.
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { 
+  
+    useNewUrlParser: true, useUnifiedTopology: true 
+
+  },err => { 
+    if(err){
+        console.log('Error un able Connected to MongoDB!!!')
+    }
+    else{
+        console.log('Connected to MongoDB!!!')
+    }
+    }
+)
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully"); 
+})
+
+
+// Access token  ====================================================================================================================  
+// ==================================================================================================================================
+app.get('/access_token', access, (req, res) => { 
+    res.status(200).json({access_token: req.access_token})
+})
+
+// Register URL  ====================================================================================================================  
+// ==================================================================================================================================
+app.get('/register_url', access, (req, res) => {
+    let url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+    let auth = "Bearer " + req.access_token
+
+    request(
+        {
+            url: url,
+            method: "POST",
+            headers: {
+                "Authorization": auth
+            },
+            json: {
+                "ShortCode": "600991",
+                "ResponseType": "Complete",
+                "ConfirmationURL": "http://197.248.86.122:801/confirmation",
+                "ValidationURL": "http://197.248.86.122:801/validation"
+            }
+        },
+        function (error, response, body) {
+            if (error) { console.log(error) }
+            res.status(200).json(body)
+        }
+    )
+})
+
+// Confirmation  ====================================================================================================================  
+// ==================================================================================================================================
+app.post('/confirmation', (req, res) => {
+    console.log('....................... confirmation .............')
+    console.log(req.body)
+})
+
+// Validation  ======================================================================================================================  
+// ==================================================================================================================================
+app.post('/validation', (req, res) => {
+    console.log('....................... validation .............')
+    console.log(req.body)
+})
+
+// Simulate  ========================================================================================================================  
+// ==================================================================================================================================
+app.get('/simulate', access, (req, res) => {
+    let url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+    let auth = "Bearer " + req.access_token
+
+    request(
+        {
+            url: url,
+            method: "POST",
+            headers: {
+                "Authorization": auth
+            },
+            json: {
+                "ShortCode": "600991",
+                "CommandID": "CustomerPayBillOnline",
+                "Amount": "100",
+                "Msisdn": "254724628580",
+                "BillRefNumber": "00000"
+            }
+        },
+        function (error, response, body) {
+            if (error) {
+                console.log(error)
+            }
+            else {
+                res.status(200).json(body)
+            }
+        }
+    )
+})
+
+// Balance  ==========================================================================================================================  
+// ===================================================================================================================================
+app.get('/balance', access, (req, resp) => {
+    let url = "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query"
+    let auth = "Bearer " + req.access_token
+
+    request(
+        {
+            url: url,
+            method: "POST",
+            headers: {
+                "Authorization": auth
+            },
+            json: {
+                "Initiator": "apitest342",
+                "SecurityCredential": "JRWM6PlYNSpshLKFIPSxldqdSGCgqKzEi89pV6T0+g8lnqaNTRVXxNoGppo8eK0IgYH+ckbAP9ii8/kCsaCPKUR0+vuHCATevlsm2nXzoLWMA2SLenFfHQ5G6go+FvWrBTX6atpok4i7iVloTgzjM3gEEBJ5zvBmimpOD1sd5JM9jnTe5q4e5MvNevgMGo5S7h0Yc/XH2Ls8Dukets32ua89kABlIPtUEb4MnHw0kztjo658nBKF13sy5mt93PW9m4+gWS3pSDroNC2KeplyBLrdDtdCQr7ef5QDxIAZ3QPCgrIytrN0aGIApjWVpYzzJazP5EIq2mrnfI7DlNoCrQ==",
+                "CommandID": "AccountBalance",
+                "PartyA": "600991",
+                "IdentifierType": "4",
+                "Remarks": "bal",
+                "QueueTimeOutURL": "http://197.248.86.122:801/bal_timeout",
+                "ResultURL": "http://197.248.86.122:801/bal_result"
+            }
+        },
+        function (error, response, body) {
+            if (error) {
+                console.log(error)
+            }
+            else {
+                resp.status(200).json(body)
+            }
+        }
+    )
+})
+
+
+function fullDate() {
+    var date = new Date(),
+      mnth = ("0" + (date.getMonth() + 1)).slice(-2)
+      day = ("0" + date.getDate()).slice(-2)
+      hours = ("0"+date.getHours()).slice(-2)
+      min =("0"+date.getMinutes()).slice(-2)
+      secs = ("0"+date.getSeconds()).slice(-2)
+    return date.getFullYear()+""+[mnth+""+day+""+hours+""+min+""+secs].join(",");
+}
+// 20220201174015
+
+// LIPA NA MPESA C2B  ==========================================================================================================================  
+// ===================================================================================================================================
+app.get('/stk', access, (req, res) => {
+    
+    const endpoint = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    let auth = "Bearer " + req.access_token
+    let date = new Date()
+    const phoneNo = "254703553986"
+    const paybbilNo = "174379"
+    const transactionType = "CustomerPayBillOnline"
+    const transAmount ="1"
+    const callBackURL ="https://d1c1-41-90-177-197.ngrok.io/stk_callback"
+    const timestamp = fullDate() 
+    const accountReference = "123Test"
+    const transactionDesc = "paybill"
+    const password = new Buffer.from('174379' + 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919' + timestamp).toString('base64')
+    
+    request(
+        {
+            url: endpoint,
+            method: "POST",
+            headers: {
+                "Authorization": auth
+            },
+            json: {   
+                "BusinessShortCode":paybbilNo,    
+                "Password":password,   
+                "Timestamp":timestamp,    
+                "TransactionType":transactionType,    
+                "Amount":transAmount,    
+                "PartyA":phoneNo,    
+                "PartyB":paybbilNo,    
+                "PhoneNumber":phoneNo,    
+                "CallBackURL":callBackURL,    
+                "AccountReference":accountReference,    
+                "TransactionDesc":transactionDesc
+            }
+        },
+        function (error, response, body) {
+            if (error) { 
+                console.log(error)
+            }
+            else { 
+                res.status(200).json(body)
+            }
+        }
+    )
+})
+
+
+app.post('/bal_result', (req, res) => {
+    console.log('.......... Account Balance ..................')
+    console.log(req.body)
+})
+
+app.post('/bal_timeout', (req, res) => {
+    console.log('.......... Timeout..................')
+    console.log(req.body)
+})
+
+app.post('/stk_callback', (req, res) => {
+    console.log('.......... STK Callback ..................')
+    console.log(req.body.Body)
+})
+// Access token  ==================================================================================================================== 
+// ================================================================================================================================== 
+function access(req, res, next){
+    
+    let url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" 
+    let auth = new Buffer.from("Bjf0WQCCV6cT1sGUn2TDp9nmMP0aUcxS:Zcg4SEOP4fxTYbKK").toString('base64')
+    
+    request(
+        {
+            url: url, 
+            headers: {
+                'Authorization': 'Basic '+auth+'' 
+            } 
+        },
+        function (error, response, body) {
+            if (error) { 
+                console.log(error) 
+            }
+            else{ 
+                req.access_token = JSON.parse(body).access_token
+                next()
+                // res.json(JSON.parse(body))
+            }
+        }
+    )
+
+}
+
+
+// Port 
+app.listen(port) 
